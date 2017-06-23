150de5e9673c3d0e8ada5a122c7f35cd4308936f
zhouming <ming.zhou@qunar.com>
2017-06-23 12:02:07 +0800

    设置为默认地址

--- js/address.js
+++ js/address.js
@@ -38,14 +38,29 @@ new Vue({
                 this.limitAddr=2;
             }
         },
-        setDefault: function(addressId){
-            this.addressList.forEach(function(item,index){
-                if(item.addressId==addressId){
-                    item.isDefault = true;
-                }else{
+
+        // 设置默认地址
+        setDefault: function(item){
+            // this.addressList.forEach(function(item,index){
+            //     if(item.addressId==addressId){
+            //
+            //     }else{
+            //         item.isDefault = false;
+            //     }
+            // });
+
+            this.$http.post("../data/setDefaultAddress.json", {addressId: item.addressId}).then(function(res){
+
+                // 这里感觉写的不太合适，不过能实现功能
+                this.addressList.forEach(function(item){
                     item.isDefault = false;
-                }
-            })
+                });
+
+                // 此处应该是有弹窗，提示成功，先用log代替吧,不知道为啥json是个字符串，只能先转成对象去获取
+                console.log(JSON.parse(res.data).msg);
+                item.isDefault = true;
+            });
+
         },
         moveAddr:function(item){
             var index = this.addressList.indexOf(item);
